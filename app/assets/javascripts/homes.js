3ed8c7623a8305bdc66c589897cf446e0bd02b00
Prashant Pattawi <prashantp@qubole.com>
2019-09-01 03:52:39 +0530

    adding first commit

---
+++ app/assets/javascripts/homes.js
@@ -0,0 +1,63 @@
+/*!
+ * classie - class helper functions
+ * from bonzo https://github.com/ded/bonzo
+ *
+ * classie.has( elem, 'my-class' ) -> true/false
+ * classie.add( elem, 'my-new-class' )
+ * classie.remove( elem, 'my-unwanted-class' )
+ * classie.toggle( elem, 'my-class' )
+ */
+
+/*jshint browser: true, strict: true, undef: true */
+/*global define: false */
+
+$(document).ready(function(){
+	$('#submit-button').click(function(e){
+		e.preventDefault();
+		e.stopPropagation();
+		var data = {};
+		for(var i=1;i<24;i++){
+			data["input-"+i] = $('#input-'+i).val();
+		}
+		var request = $.ajax({
+			async: true,
+			type: 'POST',
+			url: 'hacknight.com:3000/get_treatment_status',
+			data: data,
+			dataType: "json",
+			cache: false,
+			beforeSend: function (xhr, settings) {
+				console.log(data);
+			},
+			success: function(data, statusText) {
+				// if(dataStoreUrl.indexOf('db_sinks') > -1){
+				// 	showDataConnectionMenuList(false);
+				// 	saveOrUpdateConnectionCallBack(data, 'connection');
+				// }
+				// else{
+				// 	if(dataStoreRequestType == "PUT"){
+				// 		saveOrUpdateCallBackForConnectionTemplates(data, 'access', statusText);
+				// 	}
+				// 	else{
+				// 		saveOrUpdateCallBackForConnectionTemplates(data, 'connection', statusText);
+				// 	}
+				// }
+			},
+			error:function(xhr_obj, text_status){
+				// var statusText = xhr_obj.statusText;
+				// if(typeof statusText != 'undefined' && statusText == "error") {
+				// 	$('#connection-details-message').addClass('alert-error').show();
+				// 	$('#connection-details-message-text').html(QUBOLE.MESSAGES.DATASTORE_CREATE_ERROR + " " + QUBOLE.MESSAGES.TRY_AGAIN);
+				// } else if(typeof statusText != 'undefined') {
+				// 	$('#connection-details-message').addClass('alert-error').show();
+				// 	$('#connection-details-message-text').html(statusText + ". "+QUBOLE.MESSAGES.TRY_AGAIN+"");
+				// }
+			},
+			complete: function(xhr, textStatus) {
+				// $('#frmAddRepository').find('input,button,select').removeAttr('disabled');
+				// $('#rightContainer').animate({scrollTop: 0}, 'fast');
+				// $('#saveLoading').hide();
+			}
+		});
+	});
+});
---
+++ app/controllers/homes_controller.rb
@@ -0,0 +1,55 @@
+class HomesController < ApplicationController
+
+  # GET /homes
+  # GET /homes.json
+  def index
+    respond_to do |format|
+      format.html { render(:file => 'homes/index.html.erb')}
+    end
+  end
+
+  # GET /homes/1
+  # GET /homes/1.json
+  def show
+  end
+
+  # GET /homes/new
+  def new
+    @home = Home.new
+  end
+
+  # GET /homes/1/edit
+  def edit
+  end
+
+  # POST /homes
+  # POST /homes.json
+  def create
+    byebug
+  end
+
+  # PATCH/PUT /homes/1
+  # PATCH/PUT /homes/1.json
+  def update
+    respond_to do |format|
+      if @home.update(home_params)
+        format.html { redirect_to @home, notice: 'Home was successfully updated.' }
+        format.json { render :show, status: :ok, location: @home }
+      else
+        format.html { render :edit }
+        format.json { render json: @home.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /homes/1
+  # DELETE /homes/1.json
+  def destroy
+    @home.destroy
+    respond_to do |format|
+      format.html { redirect_to homes_url, notice: 'Home was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+end
